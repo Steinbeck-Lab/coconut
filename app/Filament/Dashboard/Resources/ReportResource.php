a8e242e3035d161cb91ae1084566205c26838112
Sagar <sriramkanakam87@gmail.com>
2024-04-14 12:15:25 +0200

    feat: created Reports resource

---
+++ app/Filament/Dashboard/Resources/ReportResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources;
+
+use App\Filament\Dashboard\Resources\ReportResource\Pages;
+use App\Filament\Dashboard\Resources\ReportResource\RelationManagers;
+use App\Models\Report;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\TextArea;
+
+class ReportResource extends Resource
+{
+    protected static ?string $navigationGroup = 'Data';
+    
+    protected static ?string $model = Report::class;
+
+    protected static ?int $navigationSort = 4;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            TextInput::make('title'),
+            TextArea::make('evidence'),
+            TextInput::make('url'),
+        ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')->wrap(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListReports::route('/'),
+            'create' => Pages\CreateReport::route('/create'),
+            'edit' => Pages\EditReport::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Dashboard/Resources/ReportResource/Pages/CreateReport.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\ReportResource\Pages;
+
+use App\Filament\Dashboard\Resources\ReportResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateReport extends CreateRecord
+{
+    protected static string $resource = ReportResource::class;
+}
