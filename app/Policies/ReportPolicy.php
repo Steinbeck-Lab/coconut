89e4349cdc442c8d786e6a50040907e68fe1ef84
Sagar <sriramkanakam87@gmail.com>
2024-04-16 03:24:09 +0200

    feat: created a ReportPolicy to fine tune authorisations (customisation is still pending)

---
+++ app/Policies/ReportPolicy.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Report;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ReportPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return true;
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Report $report): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return true;
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Report $report): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return true;
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Report $report): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Report $report): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Report $report): bool
+    {
+        if($user->hasRole('super_admin')) {
+            return false;
+        }
+        return false;
+    }
+}
