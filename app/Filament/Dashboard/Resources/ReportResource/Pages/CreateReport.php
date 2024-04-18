28d01b395b12218ceb59c7e6711b094e7c363ba5
Sagar <sriramkanakam87@gmail.com>
2024-04-18 21:14:17 +0200

    fix: added checks for the creation of report

--- app/Filament/Dashboard/Resources/ReportResource/Pages/CreateReport.php
+++ app/Filament/Dashboard/Resources/ReportResource/Pages/CreateReport.php
@@ -4,6 +4,9 @@ namespace App\Filament\Dashboard\Resources\ReportResource\Pages;
 
 use App\Filament\Dashboard\Resources\ReportResource;
 use Filament\Resources\Pages\CreateRecord;
+use App\Events\ReportSubmitted;
+use Filament\Notifications\Notification;
+use Filament\Actions\Action;
 
 class CreateReport extends CreateRecord
 {
@@ -15,5 +18,24 @@ class CreateReport extends CreateRecord
         $data['status'] = 'pending';
 
         return $data;
+    }
+
+    protected function beforeCreate(): void
+    {
+        if (!($this->data['collections'] || $this->data['citations']) ) {
+            Notification::make()
+                ->danger()
+                ->title('Select at least one Collection or Citation or Molecule.')
+                // ->body('Choose a plan to continue.')
+                ->persistent()
+                ->send();
+        
+            $this->halt();
+        }
+    }
+
+    protected function afterCreate(): void
+    {
+        ReportSubmitted::dispatch($this->record);
     }
 }
