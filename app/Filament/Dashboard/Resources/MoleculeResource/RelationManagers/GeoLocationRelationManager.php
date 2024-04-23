0cb76529ed55eb14d88031c7c884fdf59fcc68a9
Sagar <sriramkanakam87@gmail.com>
2024-04-23 16:53:37 +0200

    feat: created relationship managers for geolocation and organism

---
+++ app/Filament/Dashboard/Resources/MoleculeResource/RelationManagers/GeoLocationRelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\MoleculeResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GeoLocationRelationManager extends RelationManager
+{
+    protected static string $relationship = 'geoLocations';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\AttachAction::make()
+                    ->multiple(),
+            ])
+            ->actions([
+                Tables\Actions\DetachAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DetachBulkAction::make(),
+                ]),
+            ]);
+    }
+}
