c6a1489c729b6577931e6fc78f585ca1463aeb85
Sagar <sriramkanakam87@gmail.com>
2024-05-14 09:30:00 +0200

    chore: ran pint

--- app/Console/Commands/DashWidgetsRefresh.php
+++ app/Console/Commands/DashWidgetsRefresh.php
@@ -2,14 +2,14 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Cache;
 use App\Models\Citation;
 use App\Models\Collection;
 use App\Models\GeoLocation;
+use App\Models\Molecule;
 use App\Models\Organism;
 use App\Models\Report;
-use App\Models\Molecule;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
 
 class DashWidgetsRefresh extends Command
 {
