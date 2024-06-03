2ce5b9d079857224bf4fd39a43ce96156c07c19a
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2024-06-03 22:33:58 +0200

    fix: minor molecule card updates

--- resources/views/livewire/molecule-card.blade.php
+++ resources/views/livewire/molecule-card.blade.php
@@ -5,7 +5,7 @@
             <div class="aspect-h-3 aspect-w-3 sm:aspect-none group-hover:opacity-75 h-56">
                 <livewire:molecule-depict2d :smiles="$molecule->canonical_smiles">
             </div>
-            <div class="flex flex-1 border-t flex-col space-y-2 p-4">
+            <div class="flex flex-1 border-t flex-col p-4">
                  <div class="flex items-center">
                     @for ($i = 0; $i < $molecule->annotation_level; $i++)
                         <span class="text-amber-300">★<span>
@@ -15,9 +15,9 @@
                     @endfor
                 </div>
                 <div class="flex flex-1 flex-col justify-end">
-                    <p class="text-base font-medium text-gray-500">{{ $molecule->identifier }}</p>
+                    <p class="text-sm font-medium text-gray-500">{{ $molecule->identifier }}</p>
                 </div>
-                <h3 class="text-sm font-bold text-gray-900 capitalize">
+                <h3 class="mt-1 text-base font-bold text-gray-900 capitalize">
                     @if($molecule->name)
                         {{ $molecule->name }}
                     @else
--- resources/views/livewire/structure-editor.blade.php
+++ resources/views/livewire/structure-editor.blade.php
@@ -72,7 +72,7 @@
                         @click="Livewire.dispatch('updateSmiles', { smiles: window.getEditorSmiles(), searchType: searchType }); isOpen = false"
                         type="button"
                         class="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                        Get SMILES
+                        Search
                     </button>
                 </div>
             </div>
