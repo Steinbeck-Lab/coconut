8d2b94782698dc9d8d55b50fea2cd6cadf58941d
Sagar <sriramkanakam87@gmail.com>
2024-08-26 20:07:37 +0200

    feat: sample location relation manager for organism is now available

---
+++ app/Filament/Dashboard/Resources/OrganismResource/RelationManagers/SampleLocationsRelationManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Dashboard\Resources\OrganismResource\RelationManagers;
+
+use App\Models\SampleLocation;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SampleLocationsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'sampleLocations';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\Action::make('edit')
+                    ->url(fn (SampleLocation $record) => env('APP_URL').'/dashboard/sample-locations/'.$record->id.'/edit')
+                    // ->color('info')
+                    ->icon('heroicon-m-pencil-square'),
+                // Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
