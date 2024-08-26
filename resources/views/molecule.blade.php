ba5ccab3050d86f341b48692adccf68f10013650
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2024-08-26 15:21:19 +0200

    fix: updated twitter card

--- resources/views/molecule.blade.php
+++ resources/views/molecule.blade.php
@@ -2,7 +2,7 @@
     @section('title', $molecule['identifier'])
     @section('meta')
         <meta name="description"
-            content="Molecule details for {{ $molecule->name ? $molecule->name : $molecule->iupac_name }}">
+        content="Molecule details for {{ $molecule->name ? $molecule->name : $molecule->iupac_name }}">
         <meta name="keywords" content="{{ implode(',', $molecule->synonyms ?? []) }}">
         <meta name="author" content="COCONUT">
         <meta property="og:title" content="{{ $molecule['identifier'] }} - COCONUT: COlleCtion of Open Natural prodUcTs">
@@ -15,6 +15,14 @@
         <meta property="og:image:width" content="1200">
         <meta property="og:image:height" content="630">
         <meta property="og:site_name" content="{{ config('app.name', 'COCONUT') }}">
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{ $molecule['identifier'] }} - COCONUT: COlleCtion of Open Natural prodUcTs">
+        <meta name="twitter:description"
+            content="Molecule details for {{ $molecule->name ? $molecule->name : $molecule->iupac_name }}">
+        <meta name="twitter:image" content="{{ env('CM_API') . 'depict/2D?smiles=' . urlencode($molecule->canonical_smiles) . '&height=630&width=1200&toolkit=cdk' ?? asset('img/coconut-og-image.png') }}">
+        <meta name="twitter:site" content="@coconutdatabase">
+        <meta name="twitter:creator" content="@coconutdatabase">
     @overwrite
     @section('schema')
         @if (isset($molecule['schema']))
