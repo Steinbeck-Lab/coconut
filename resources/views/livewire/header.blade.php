87295ca2214834249231f15537d20cf9ed4dead1
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2024-08-26 14:48:35 +0200

    fix: added cache policy for images and also updated links in application logo

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,9 +1,9 @@
     <header class="absolute inset-x-0 top-0" x-data="{ open: false }">
         <nav class="flex items-center justify-between p-6 lg:px-8 max-w-4xl lg:max-w-7xl mx-auto" aria-label="Global">
             <div class="flex lg:flex-1">
-                <a href="/" class="-m-1.5 p-1.5">
+                <div class="-m-1.5 p-1.5">
                     <x-authentication-card-logo />
-                </a>
+                </div>
             </div>
             <div class="flex lg:hidden">
                 <button type="button" @click="open = true"
@@ -100,9 +100,9 @@
             <div
                 class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
-                    <a href="/" class="-m-1.5 p-1.5">
+                    <div class="-m-1.5 p-1.5">
                         <x-authentication-card-logo />
-                    </a>
+                    </div>
                     <button type="button" @click="open = false" class="-m-2.5 rounded-md p-2.5 text-gray-700">
                         <span class="sr-only">Close menu</span>
                         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
