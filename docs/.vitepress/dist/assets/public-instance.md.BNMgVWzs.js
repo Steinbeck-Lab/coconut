38d9c4a7aa92b628e70a38c741141f7b41e7c2b7
Sagar <sriramkanakam87@gmail.com>
2024-09-02 14:19:02 +0200

    chore: updates to local and public instance documentation

--- docs/.vitepress/dist/assets/public-instance.md.BNMgVWzs.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.CpPNxFD6.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"public-instance.md","filePath":"public-instance.md"}'),c={name:"public-instance.md"};function n(s,r,i,o,p,l){return a(),t("div")}const m=e(c,[["render",n]]);export{d as __pageData,m as default};
