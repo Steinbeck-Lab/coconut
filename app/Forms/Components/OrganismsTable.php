60a840da879da515efa42a5d6d642b468afdc699
Sagar <sriramkanakam87@gmail.com>
2024-09-05 13:24:42 +0200

    fix: now only the similar organism names with any molecules attached to them are displayed in the organism edit page's side panel

--- app/Forms/Components/OrganismsTable.php
+++ app/Forms/Components/OrganismsTable.php
@@ -16,8 +16,12 @@ class OrganismsTable extends Field
 
     public function getTableData($record_name)
     {
-        return Organism::select('id', 'name', urldecode('iri'), 'molecule_count')->where('name', 'ILIKE', '%'.$record_name.'%')
-            ->orWhereRaw('? ILIKE CONCAT(\'%\', name, \'%\')', [$record_name])
+        return Organism::select('id', 'name', urldecode('iri'), 'molecule_count')
+            ->where('molecule_count', '>', 0)
+            ->where(function ($q) use ($record_name) {
+                $q->where('name', 'ILIKE', '%' . $record_name . '%')
+                ->orWhereRaw('? ILIKE CONCAT(\'%\', name, \'%\')', [$record_name]);
+            })
             ->get();
     }
 }
