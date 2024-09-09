01ad02ddf5dc414a68b32a727cef5bf52f3768a5
Sagar <sriramkanakam87@gmail.com>
2024-09-09 19:45:05 +0200

    fix: improvements to the time line display in the compound details page

--- app/Livewire/MoleculeHistoryTimeline.php
+++ app/Livewire/MoleculeHistoryTimeline.php
@@ -18,19 +18,17 @@ class MoleculeHistoryTimeline extends Component
             $audit_data[$index]['event'] = $audit->getMetadata()['audit_event'];
             $audit_data[$index]['created_at'] = date('Y/m/d', strtotime($audit->getMetadata()['audit_created_at']));
             foreach ($audit->getModified() as $key => $value) {
-                $audit_data[$index]['affected_column'] = $key;
-                $audit_data[$index]['old_value'] = $value['old'];
-                $audit_data[$index]['new_value'] = $value['new'];
+                $audit_data[$index]['affected_columns'][$key]['old_value'] = $value['old'];
+                $audit_data[$index]['affected_columns'][$key]['new_value'] = $value['new'];
             }
         }
 
         $initial_audit = [];
         $initial_audit['user_name'] = null;
-        $initial_audit['event'] = 'created';
+        $initial_audit['event'] = null;
         $initial_audit['created_at'] = $this->mol->created_at->format('Y/m/d');
-        $initial_audit['affected_column'] = null;
-        $initial_audit['old_value'] = null;
-        $initial_audit['new_value'] = null;
+        $initial_audit['affected_columns']['created']['old_value'] = null;
+        $initial_audit['affected_columns']['created']['new_value'] = null;
 
         array_unshift($audit_data, $initial_audit);
         $this->audit_data = $audit_data;
