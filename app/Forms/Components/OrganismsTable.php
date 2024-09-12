1999287985b9cc9a5a1ba1a554e342943277b7ea
Sagar <sriramkanakam87@gmail.com>
2024-09-12 14:42:44 +0200

    fix: now similar organisms list excludes the same organism name

--- app/Forms/Components/OrganismsTable.php
+++ app/Forms/Components/OrganismsTable.php
@@ -19,8 +19,10 @@ class OrganismsTable extends Field
         return Organism::select('id', 'name', urldecode('iri'), 'molecule_count')
             ->where('molecule_count', '>', 0)
             ->where(function ($q) use ($record_name) {
-                $q->where('name', 'ILIKE', '%'.$record_name.'%')
-                    ->orWhereRaw('? ILIKE CONCAT(\'%\', name, \'%\')', [$record_name]);
+                $q->where([
+                    ['name', '!=', $record_name],
+                    ['name', 'ILIKE', '%'.$record_name.'%']
+                ]);
             })
             ->get();
     }
