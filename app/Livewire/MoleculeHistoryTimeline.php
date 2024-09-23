2da1fcfdd08dc7e5ca7523b18ac51a0d2cacf81f
Sagar <sriramkanakam87@gmail.com>
2024-09-24 00:57:48 +0200

    fix: key issue fixed

--- app/Livewire/MoleculeHistoryTimeline.php
+++ app/Livewire/MoleculeHistoryTimeline.php
@@ -18,18 +18,17 @@ class MoleculeHistoryTimeline extends Component
             $audit_data[$index]['user_name'] = $audit->getMetadata()['user_name'];
             $audit_data[$index]['event'] = $audit->getMetadata()['audit_event'];
             $audit_data[$index]['created_at'] = date('Y/m/d', strtotime($audit->getMetadata()['audit_created_at']));
+            // dd($audit->old_values, $audit->new_values);
+            // dd($audit->getModified());
 
-            if (str_contains('.', array_keys($audit->old_values)[0])) {
-                $old_key = $audit->old_values ? explode('.', array_keys($audit->old_values)[0])[0] : null;
-                $new_key = $audit->new_values ? explode('.', array_keys($audit->new_values)[0])[0] : null;
+            $key = null;
+            $old_key = $audit->old_values ? explode('.', array_keys($audit->old_values)[0])[0] : null;
+            $new_key = $audit->new_values ? explode('.', array_keys($audit->new_values)[0])[0] : null;
+            $key = $old_key ?? $new_key;
 
-                $old_key = $old_key ?: $new_key;
-                $new_key = $new_key ?: $old_key;
-            } else {
-                foreach ($audit->getModified() as $key => $value) {
-                    $audit_data[$index]['affected_columns'][$key]['old_value'] = $value['old'] ?: null;
-                    $audit_data[$index]['affected_columns'][$key]['new_value'] = $value['new'] ?: null;
-                }
+            foreach ($audit->getModified() as $value) {
+                $audit_data[$index]['affected_columns'][$key]['old_value'] = array_key_exists('old', $value) ? $value['old'] : null;
+                $audit_data[$index]['affected_columns'][$key]['new_value'] = array_key_exists('new', $value) ? $value['new'] : null;
             }
         }
 
