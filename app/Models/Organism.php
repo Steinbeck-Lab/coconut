51510be16f28d5132587473e5e02c7d56001c86b
Sagar <sriramkanakam87@gmail.com>
2024-09-26 17:35:19 +0200

    chore: commented out the slide over (wip)

--- app/Models/Organism.php
+++ app/Models/Organism.php
@@ -62,88 +62,89 @@ class Organism extends Model implements Auditable
                 ->required()
                 ->unique(Organism::class, 'name')
                 ->maxLength(255)
-                ->suffixAction(
-                    Action::make('infoFromSources')
-                        ->icon('heroicon-m-clipboard')
-                        // ->fillForm(function ($record, callable $get): array {
-                        //     $entered_name = $get('name');
-                        //     $name = ucfirst(trim($entered_name));
-                        //     $data = null;
-                        //     $iri = null;
-                        //     $organism = null;
-                        //     $rank = null;
+            // ->suffixAction(
+            //     Action::make('infoFromSources')
+            //         ->icon('heroicon-m-clipboard')
+            //         // ->fillForm(function ($record, callable $get): array {
+            //         //     $entered_name = $get('name');
+            //         //     $name = ucfirst(trim($entered_name));
+            //         //     $data = null;
+            //         //     $iri = null;
+            //         //     $organism = null;
+            //         //     $rank = null;
 
-                        //     if ($name && $name != '') {
-                        //         $data = Self::getOLSIRI($name, 'species');
-                        //         if ($data) {
-                        //             Self::updateOrganismModel($name, $data, $record, 'species');
-                        //             Self::info("Mapped and updated: $name");
-                        //         } else {
-                        //             $data = Self::getOLSIRI(explode(' ', $name)[0], 'genus');
-                        //             if ($data) {
-                        //                 Self::updateOrganismModel($name, $data, $record, 'genus');
-                        //                 Self::info("Mapped and updated: $name");
-                        //             } else {
-                        //                 $data = Self::getOLSIRI(explode(' ', $name)[0], 'family');
-                        //                 if ($data) {
-                        //                     Self::updateOrganismModel($name, $data, $record, 'genus');
-                        //                     Self::info("Mapped and updated: $name");
-                        //                 } else {
-                        //                     [$name, $iri, $organism, $rank] = Self::getGNFMatches($name, $record);
-                        //                 }
-                        //             }
-                        //         }
-                        //     }
-                        //     return [
-                        //         'name' => $name,
-                        //         'iri' => $iri,
-                        //         'rank' => $rank,
-                        //     ];
-                        // })
-                        // ->form([
-                        //     Forms\Components\TextInput::make('name')->readOnly(),
-                        //     Forms\Components\TextInput::make('iri')->readOnly(),
-                        //     Forms\Components\TextInput::make('rank')->readOnly(),
-                        // ])
-                        // ->action(fn ( $record) => $record->advance())
-                        ->modalContent(function ($record, $get): View {
-                            $name = ucfirst(trim($get('name')));
-                            $data = null;
-                            // $iri = null;
-                            // $organism = null;
-                            // $rank = null;
+            //         //     if ($name && $name != '') {
+            //         //         $data = Self::getOLSIRI($name, 'species');
+            //         //         if ($data) {
+            //         //             Self::updateOrganismModel($name, $data, $record, 'species');
+            //         //             Self::info("Mapped and updated: $name");
+            //         //         } else {
+            //         //             $data = Self::getOLSIRI(explode(' ', $name)[0], 'genus');
+            //         //             if ($data) {
+            //         //                 Self::updateOrganismModel($name, $data, $record, 'genus');
+            //         //                 Self::info("Mapped and updated: $name");
+            //         //             } else {
+            //         //                 $data = Self::getOLSIRI(explode(' ', $name)[0], 'family');
+            //         //                 if ($data) {
+            //         //                     Self::updateOrganismModel($name, $data, $record, 'genus');
+            //         //                     Self::info("Mapped and updated: $name");
+            //         //                 } else {
+            //         //                     [$name, $iri, $organism, $rank] = Self::getGNFMatches($name, $record);
+            //         //                 }
+            //         //             }
+            //         //         }
+            //         //     }
+            //         //     return [
+            //         //         'name' => $name,
+            //         //         'iri' => $iri,
+            //         //         'rank' => $rank,
+            //         //     ];
+            //         // })
+            //         // ->form([
+            //         //     Forms\Components\TextInput::make('name')->readOnly(),
+            //         //     Forms\Components\TextInput::make('iri')->readOnly(),
+            //         //     Forms\Components\TextInput::make('rank')->readOnly(),
+            //         // ])
+            //         // ->action(fn ( $record) => $record->advance())
+            //         ->modalContent(function ($record, $get): View {
+            //             $name = ucfirst(trim($get('name')));
+            //             $data = null;
+            //             // $iri = null;
+            //             // $organism = null;
+            //             // $rank = null;
 
-                            if ($name && $name != '') {
-                                $data = self::getOLSIRI($name, 'species');
-                                // if ($data) {
-                                //     Self::updateOrganismModel($name, $data, $record, 'species');
-                                // } else {
-                                //     $data = Self::getOLSIRI(explode(' ', $name)[0], 'genus');
-                                //     if ($data) {
-                                //         Self::updateOrganismModel($name, $data, $record, 'genus');
-                                //     } else {
-                                //         $data = Self::getOLSIRI(explode(' ', $name)[0], 'family');
-                                //         if ($data) {
-                                //             Self::updateOrganismModel($name, $data, $record, 'genus');
-                                //         } else {
-                                //             [$name, $iri, $organism, $rank] = Self::getGNFMatches($name, $record);
-                                //         }
-                                //     }
-                                // }
-                            }
+            //             if ($name && $name != '') {
+            //                 $data = self::getOLSIRI($name, 'species');
+            //                 // if ($data) {
+            //                 //     Self::updateOrganismModel($name, $data, $record, 'species');
+            //                 // } else {
+            //                 //     $data = Self::getOLSIRI(explode(' ', $name)[0], 'genus');
+            //                 //     if ($data) {
+            //                 //         Self::updateOrganismModel($name, $data, $record, 'genus');
+            //                 //     } else {
+            //                 //         $data = Self::getOLSIRI(explode(' ', $name)[0], 'family');
+            //                 //         if ($data) {
+            //                 //             Self::updateOrganismModel($name, $data, $record, 'genus');
+            //                 //         } else {
+            //                 //             [$name, $iri, $organism, $rank] = Self::getGNFMatches($name, $record);
+            //                 //         }
+            //                 //     }
+            //                 // }
+            //             }
 
-                            return view(
-                                'forms.components.organism-info',
-                                [
-                                    'data' => $data,
-                                ],
-                            );
-                        })
-                        ->action(function (array $data, Organism $record): void {
-                            // Self::updateOrganismModel($data['name'], $data['iri'], $record, $data['rank']);
-                        })
-                        ->slideOver()
-                ),
+            //             return view(
+            //                 'forms.components.organism-info',
+            //                 [
+            //                     'data' => $data,
+            //                 ],
+            //             );
+            //         })
+            //         ->action(function (array $data, Organism $record): void {
+            //             // Self::updateOrganismModel($data['name'], $data['iri'], $record, $data['rank']);
+            //         })
+            //         ->slideOver()
+            // )
+            ,
             Forms\Components\TextInput::make('iri')
                 ->label('IRI')
                 ->maxLength(255),
