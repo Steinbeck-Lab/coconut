2c1149ff90d1ee831b2a4e200eb2a52e06c9b353
Sagar <sriramkanakam87@gmail.com>
2024-10-02 01:16:16 +0200

    feat: enabled create time pop-up modal to display overall changes being requested
    The data structure of suggested changes is altered to suit the needs.

--- app/Filament/Dashboard/Resources/ReportResource/Pages/CreateReport.php
+++ app/Filament/Dashboard/Resources/ReportResource/Pages/CreateReport.php
@@ -8,30 +8,46 @@ use App\Models\Citation;
 use App\Models\Collection;
 use App\Models\Molecule;
 use App\Models\Organism;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateReport extends CreateRecord
 {
     protected static string $resource = ReportResource::class;
 
+    protected $molecule;
+
     public function getTitle(): string
     {
         $title = 'Create Report';
         request()->type == 'change' ? $title = 'Request Changes' : $title = 'Report ';
         if (request()->has('compound_id')) {
-            $molecule = Molecule::where('identifier', request()->compound_id)->first();
-            $title = $title.' - '.$molecule->name.' ('.$molecule->identifier.')';
+            $title = $title.' - '.$this->molecule->name.' ('.$this->molecule->identifier.')';
         }
 
         return __($title);
     }
 
+    protected function beforeFill(): void
+    {
+        if (request()->has('compound_id')) {
+            $this->molecule = Molecule::where('identifier', request()->compound_id)->first();
+        }
+    }
+
     protected function afterFill(): void
     {
         $request = request();
+        $this->data['compound_id'] = $request->compound_id;
         if ($request->type == 'change') {
             $this->data['is_change'] = true;
+            $this->data['existing_geo_locations'] = $this->molecule->geo_locations->pluck('name')->toArray();
+            $this->data['existing_synonyms'] = $this->molecule->synonyms;
+            $this->data['existing_cas'] = array_values($this->molecule->cas);
+            $this->data['existing_organisms'] = $this->molecule->organisms->pluck('name')->toArray();
+            $this->data['existing_citations'] = $this->molecule->citations->where('title', '!=', null)->pluck('title')->toArray();
         }
+
         if ($request->has('collection_uuid')) {
             $collection = Collection::where('uuid', $request->collection_uuid)->get();
             $id = $collection[0]->id;
@@ -79,14 +95,40 @@ class CreateReport extends CreateRecord
         $data['user_id'] = auth()->id();
         $data['status'] = 'submitted';
 
-        $suggested_changes = [];
-        $suggested_changes['organisms_changes'] = $data['organisms_changes'];
-        $suggested_changes['geo_locations_changes'] = $data['geo_locations_changes'];
-        $suggested_changes['synonyms_changes'] = $data['synonyms_changes'];
-        $suggested_changes['identifiers_changes'] = $data['identifiers_changes'];
-        $suggested_changes['citations_changes'] = $data['citations_changes'];
+        if ($data['is_change'] == true) {
+            $suggested_changes = [];
+            $suggested_changes['existing_geo_locations'] = $data['existing_geo_locations'];
+            $suggested_changes['new_geo_locations'] = $data['new_geo_locations'];
+            $suggested_changes['approve_geo_locations'] = false;
 
-        $data['suggested_changes'] = $suggested_changes;
+            $suggested_changes['existing_synonyms'] = $data['existing_synonyms'];
+            $suggested_changes['new_synonyms'] = $data['new_synonyms'];
+            $suggested_changes['approve_synonyms'] = false;
+
+            $suggested_changes['name'] = $data['name'];
+            $suggested_changes['approve_name'] = false;
+
+            $suggested_changes['existing_cas'] = $data['existing_cas'];
+            $suggested_changes['new_cas'] = $data['new_cas'];
+            $suggested_changes['approve_cas'] = false;
+
+            $suggested_changes['existing_organisms'] = $data['existing_organisms'];
+            $suggested_changes['approve_existing_organisms'] = false;
+
+            $suggested_changes['new_organisms'] = $data['new_organisms'];
+
+            $suggested_changes['existing_citations'] = $data['existing_citations'];
+            $suggested_changes['approve_existing_citations'] = false;
+
+            $suggested_changes['new_citations'] = $data['new_citations'];
+
+            $suggested_changes['overall_changes'] = getOverallChanges($data);
+
+            // seperate copy for Curators
+            $suggested_changes['curator'] = $suggested_changes;
+
+            $data['suggested_changes'] = $suggested_changes;
+        }
 
         return $data;
     }
@@ -103,5 +145,21 @@ class CreateReport extends CreateRecord
         }
 
         ReportSubmitted::dispatch($this->record);
+    }
+
+    protected function getCreateFormAction(): Action
+    {
+        return parent::getCreateFormAction()
+            ->submit(null)
+            ->form(function () {
+                return getChangesToDisplayModal($this->data);
+            })
+            ->modalHidden(function () {
+                return ! $this->data['is_change'];
+            })
+            ->action(function () {
+                $this->closeActionModal();
+                $this->create();
+            });
     }
 }
