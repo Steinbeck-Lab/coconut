9ad664155d9567d2d85614dfc4a9068afaacb813
Sagar <sriramkanakam87@gmail.com>
2024-10-07 11:06:37 +0200

    fix: auto flush removed for dash widgets and geo-locations' widgets get the data on the fly when the page is accessed.

--- app/Console/Commands/DashWidgetsRefresh.php
+++ app/Console/Commands/DashWidgetsRefresh.php
@@ -29,52 +29,52 @@ class DashWidgetsRefresh extends Command
     public function handle()
     {
         // Clear the cache for all widgets
-        // Cache::flush();
+        Cache::flush();
 
         // Create the cache for all DashboardStats widgets
-        Cache::remember('stats.collections', 172800, function () {
+        Cache::rememberForever('stats.collections', function () {
             return DB::table('collections')->selectRaw('count(*)')->get()[0]->count;
         });
         $this->info('Cache for collections refreshed.');
 
-        Cache::remember('stats.citations', 172800, function () {
+        Cache::rememberForever('stats.citations', function () {
             return DB::table('citations')->selectRaw('count(*)')->get()[0]->count;
         });
         $this->info('Cache for citations refreshed.');
 
-        Cache::remember('stats.organisms', 172800, function () {
+        Cache::rememberForever('stats.organisms', function () {
             return DB::table('organisms')->selectRaw('count(*)')->get()[0]->count;
         });
         $this->info('Cache for organisms refreshed.');
 
-        Cache::remember('stats.geo_locations', 172800, function () {
+        Cache::rememberForever('stats.geo_locations', function () {
             return DB::table('geo_locations')->selectRaw('count(*)')->get()[0]->count;
         });
         $this->info('Cache for geo locations refreshed.');
 
-        Cache::remember('stats.reports', 172800, function () {
+        Cache::rememberForever('stats.reports',  function () {
             return DB::table('reports')->selectRaw('count(*)')->get()[0]->count;
         });
         $this->info('Cache for reports refreshed.');
 
         // Create the cache for all DashboardStatsMid widgets
 
-        Cache::remember('stats.molecules.non_stereo', 172800, function () {
+        Cache::rememberForever('stats.molecules.non_stereo',  function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('has_stereo=false and is_parent=false')->get()[0]->count;
         });
         $this->info('Cache for molecules non-stereo refreshed.');
 
-        Cache::remember('stats.molecules.stereo', 172800, function () {
+        Cache::rememberForever('stats.molecules.stereo',  function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('has_stereo=true')->get()[0]->count;
         });
         $this->info('Cache for molecules stereo refreshed.');
 
-        Cache::remember('stats.molecules.parent', 172800, function () {
+        Cache::rememberForever('stats.molecules.parent',  function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('has_stereo=false and is_parent=true')->get()[0]->count;
         });
         $this->info('Cache for molecules parent refreshed.');
 
-        Cache::remember('stats.molecules', 172800, function () {
+        Cache::rememberForever('stats.molecules', function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('active=true and NOT (is_parent=true AND has_variants=true)')->get()[0]->count;
         });
         $this->info('Cache for molecules refreshed.');
