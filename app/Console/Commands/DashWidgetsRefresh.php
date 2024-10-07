3ce816a0da60f9d49896a55416816782c0ef9f1c
Sagar <sriramkanakam87@gmail.com>
2024-10-07 11:11:09 +0200

    chore: formatting changes

--- app/Console/Commands/DashWidgetsRefresh.php
+++ app/Console/Commands/DashWidgetsRefresh.php
@@ -52,24 +52,24 @@ class DashWidgetsRefresh extends Command
         });
         $this->info('Cache for geo locations refreshed.');
 
-        Cache::rememberForever('stats.reports',  function () {
+        Cache::rememberForever('stats.reports', function () {
             return DB::table('reports')->selectRaw('count(*)')->get()[0]->count;
         });
         $this->info('Cache for reports refreshed.');
 
         // Create the cache for all DashboardStatsMid widgets
 
-        Cache::rememberForever('stats.molecules.non_stereo',  function () {
+        Cache::rememberForever('stats.molecules.non_stereo', function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('has_stereo=false and is_parent=false')->get()[0]->count;
         });
         $this->info('Cache for molecules non-stereo refreshed.');
 
-        Cache::rememberForever('stats.molecules.stereo',  function () {
+        Cache::rememberForever('stats.molecules.stereo', function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('has_stereo=true')->get()[0]->count;
         });
         $this->info('Cache for molecules stereo refreshed.');
 
-        Cache::rememberForever('stats.molecules.parent',  function () {
+        Cache::rememberForever('stats.molecules.parent', function () {
             return DB::table('molecules')->selectRaw('count(*)')->whereRaw('has_stereo=false and is_parent=true')->get()[0]->count;
         });
         $this->info('Cache for molecules parent refreshed.');
