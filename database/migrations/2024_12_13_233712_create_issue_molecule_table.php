f5f58d8a7f0e609325355d5cc0be086281d41f05
Sagar <sriramkanakam87@gmail.com>
2024-12-13 16:10:36 +0100

    feat: new model and related migrations for issues table

---
+++ database/migrations/2024_12_13_233712_create_issue_molecule_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issue_molecule', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('issue_id');
+            $table->foreignId('molecule_id');
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_fixed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issue_molecule');
+    }
+};
