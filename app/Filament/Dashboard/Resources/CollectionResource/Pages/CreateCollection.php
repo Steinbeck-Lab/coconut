cc0be42994bde23b57910c0ad0d804444983b2f6
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2025-01-15 11:19:32 +0100

    chore: published livewire assets and added slug mutation trait to the collection model

--- app/Filament/Dashboard/Resources/CollectionResource/Pages/CreateCollection.php
+++ app/Filament/Dashboard/Resources/CollectionResource/Pages/CreateCollection.php
@@ -3,9 +3,15 @@
 namespace App\Filament\Dashboard\Resources\CollectionResource\Pages;
 
 use App\Filament\Dashboard\Resources\CollectionResource;
+use App\Models\Collection;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCollection extends CreateRecord
 {
     protected static string $resource = CollectionResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        return Collection::mutateFormData($data);
+    }
 }
