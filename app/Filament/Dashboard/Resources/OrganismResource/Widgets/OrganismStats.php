e164e5fba252b52bf824abfd9ca534eb70ca194a
Sagar <sriramkanakam87@gmail.com>
2025-09-17 14:33:17 +0200

    fix: parameterised the raw queries for medium-level risks.

--- app/Filament/Dashboard/Resources/OrganismResource/Widgets/OrganismStats.php
+++ app/Filament/Dashboard/Resources/OrganismResource/Widgets/OrganismStats.php
@@ -16,10 +16,10 @@ class OrganismStats extends BaseWidget
     {
         return [
             Stat::make('Total Molecules', Cache::flexible('stats.organisms'.$this->record->id.'molecules.count', [172800, 259200], function () {
-                return DB::table('molecule_organism')->selectRaw('count(*)')->whereRaw('organism_id='.$this->record->id)->get()[0]->count;
+                return DB::table('molecule_organism')->selectRaw('count(*)')->whereRaw('organism_id=?', [$this->record->id])->get()[0]->count;
             })),
             Stat::make('Total Geo Locations', Cache::flexible('stats.organisms'.$this->record->id.'geo_locations.count', [172800, 259200], function () {
-                return DB::table('molecule_organism')->selectRaw('count(*)')->whereRaw('organism_id='.$this->record->id)->Join('geo_location_molecule', 'molecule_organism.molecule_id', '=', 'geo_location_molecule.molecule_id')->get()[0]->count;
+                return DB::table('molecule_organism')->selectRaw('count(*)')->whereRaw('organism_id=?', [$this->record->id])->Join('geo_location_molecule', 'molecule_organism.molecule_id', '=', 'geo_location_molecule.molecule_id')->get()[0]->count;
             })),
         ];
     }
