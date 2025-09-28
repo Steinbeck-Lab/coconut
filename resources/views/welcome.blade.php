f27ebd36559420f41759f4cd920f46279d4bdaf9
Sagar <sriramkanakam87@gmail.com>
2025-09-28 07:17:18 +0200

    fix: limited the banner display only to development.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     @section("title", "Welcome")
-    <div class="@if(config('app.env') !== 'production') mt-32 @else mt-20 @endif">
+    <div class="@if(config('app.env') === 'development') mt-32 @else mt-20 @endif">
         <livewire:welcome>
     </div>
 </x-guest-layout>
