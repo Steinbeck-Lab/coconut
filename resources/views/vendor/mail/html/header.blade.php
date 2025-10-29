1fbf0c1b54dcf9e24023ed79adcaab7123b1c832
Venkata Nainala <mailcs76@gmail.com>
2025-10-29 14:30:15 +0530

    fix: updated laravel default email template to use coconut logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ config('app.url') }}/img/logo.png" style="height: 56px; width: auto !important" alt="COCONUT Logo">
+</a>
+</td>
+</tr>
