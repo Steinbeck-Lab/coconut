fd22d029fb9995e16cb52640f5f4da7631b012de
Nisha Sharma <sharmanis14@gmail.com>
2025-11-20 12:34:36 +0100

    update middleware to handle HTTP routes correctly

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,20 +5,27 @@ use BezhanSalleh\FilamentExceptions\FilamentExceptions;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         apiPrefix: 'api',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // Enable proxy handling (uses App\Http\Middleware\TrustProxies)
-        $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_ALL
+        );
 
-        // Uncomment if/when you want this alias available
+        // If you later want aliases etc., you can configure them here:
         // $middleware->alias([
         //     'verified' => EnsureEmailOrPhoneIsVerified::class,
         // ]);
