249fce76b07df30dd41adb5da0c66709ed47c40f
Sagar <sriramkanakam87@gmail.com>
2025-12-02 17:06:02 +0100

    fix: InfolistSection is not there.

--- app/Filament/Dashboard/Resources/CollectionResource.php
+++ app/Filament/Dashboard/Resources/CollectionResource.php
@@ -24,7 +24,6 @@ use Filament\Forms\Components\SpatieTagsInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Infolists\Components\ImageEntry;
-use Filament\Infolists\Components\Section as InfolistSection;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Resource;
 use Filament\Schemas\Components\Grid;
@@ -144,7 +143,7 @@ class CollectionResource extends Resource
     {
         return $schema
             ->components([
-                InfolistSection::make('Database Details')
+                Section::make('Database Details')
                     ->schema([
                         TextEntry::make('title')
                             ->label('Collection Title'),
@@ -158,7 +157,7 @@ class CollectionResource extends Resource
                             ->placeholder('No URL provided'),
                     ])
                     ->columns(2),
-                InfolistSection::make('Metadata')
+                Section::make('Metadata')
                     ->schema([
                         TextEntry::make('tags.*.name')
                             ->label('Tags')
@@ -169,14 +168,14 @@ class CollectionResource extends Resource
                             ->placeholder('No identifier'),
                     ])
                     ->columns(2),
-                InfolistSection::make('Display Image')
+                Section::make('Display Image')
                     ->schema([
                         ImageEntry::make('image')
                             ->label('Collection Image')
                             ->visibility('public')
                             ->size(200),
                     ]),
-                InfolistSection::make('Distribution')
+                Section::make('Distribution')
                     ->schema([
                         TextEntry::make('license.title')
                             ->label('License')
